44a425710d4f0ea11221ca1783822ffa38ed62e8
AHTHneeuhl <ahthneeuhl@gmail.com>
2021-12-20 10:44:57 +0530

    Store and Reducer

--- src/App.js
+++ src/App.js
@@ -3,7 +3,27 @@ import Navbar from "./components/Navbar";
 import CartContainer from "./components/CartContainer";
 // items
 import cartItems from "./cart-items";
+
 // redux stuff
+import { createStore } from "redux";
+
+// initial state
+const initialState = {
+  count: 0,
+};
+
+// reducer - function that used to update state
+// take two parameters - state, action
+// state - previous state
+// action - what state to update
+// return previous state or updated state
+const reducer = (state, action) => {
+  console.log({ state, action });
+  return state;
+};
+
+// store - store data, think of state
+const store = createStore(reducer, initialState);
 
 function App() {
   // cart setup

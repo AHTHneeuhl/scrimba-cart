cc34118c06bc6d70e28bc87e5709ca9e2d44eaad
AHTHneeuhl <ahthneeuhl@gmail.com>
2021-12-20 12:01:09 +0530

    Seperate Reducer

--- src/App.js
+++ src/App.js
@@ -4,31 +4,16 @@ import CartContainer from "./components/CartContainer";
 // items
 import cartItems from "./cart-items";
 // action variables
-import { INCREMENT, DECREMENT } from "./actions";
+import { INCREMENT, DECREMENT } from "./store/actions";
 
 // redux stuff
 import { createStore } from "redux";
+import reducer from "./store/reducer";
 
 // initial state
 const initialState = {
   count: 3,
   name: "john",
-};
-
-// reducer - function that used to update state
-// take two parameters - state, action
-// state - previous state
-// action - what state to update
-// return previous state or updated state
-// DON'T MUTATE the STATE - redux built on immutability
-const reducer = (state, action) => {
-  console.log({ state, action });
-  if (action.type === DECREMENT) {
-    return { ...state, count: state.count - 1 };
-  } else if (action.type === INCREMENT) {
-    return { ...state, count: state.count + 1 };
-  }
-  return state;
 };
 
 // store - store data, think of state
---
+++ src/store/reducer.js
@@ -0,0 +1,19 @@
+import { INCREMENT, DECREMENT } from "./actions";
+
+// reducer - function that used to update state
+// take two parameters - state, action
+// state - previous state
+// action - what state to update
+// return previous state or updated state
+// DON'T MUTATE the STATE - redux built on immutability
+const reducer = (state, action) => {
+  console.log({ state, action });
+  if (action.type === DECREMENT) {
+    return { ...state, count: state.count - 1 };
+  } else if (action.type === INCREMENT) {
+    return { ...state, count: state.count + 1 };
+  }
+  return state;
+};
+
+export default reducer;

420253a4b84a8540ef29c419f8b3a94547c7c658
AHTHneeuhl <ahthneeuhl@gmail.com>
2021-12-22 17:26:04 +0530

    final finish

--- src/components/CartContainer.js
+++ src/components/CartContainer.js
@@ -12,7 +12,6 @@ const CartContainer = ({ cart = [], total, dispatch }) => {
   if (cart.length === 0) {
     return (
       <section className='cart'>
-        {/* cart header */}
         <header>
           <h2>your bag</h2>
           <h4 className='empty-cart'>is currently empty</h4>
@@ -23,17 +22,14 @@ const CartContainer = ({ cart = [], total, dispatch }) => {
 
   return (
     <section className='cart'>
-      {/* cart header */}
       <header>
         <h2>your bag</h2>
       </header>
-      {/* cart items */}
       <article>
         {cart.map((item) => {
           return <CartItem key={item.id} {...item} />;
         })}
       </article>
-      {/* cart footer */}
       <footer>
         <hr />
         <div className='cart-total'>
